5dabbabcbc6a12cb3c0cb52af5a669bd37d01af1
miq28 <iqbal.tiro@gmail.com>
2026-03-30 22:31:29 +0700

    Pinout for Waveshare ESP3-S3-CAN-485 Board

--- src/config.h
+++ src/config.h
@@ -26,11 +26,28 @@
 #define KEY GPIO_NUM_0
 
 #elif defined(WAVESHARE_ESP32_S3_RS485_CAN)
+// CAN / TWAI
 #define CAN_TX GPIO_NUM_15
 #define CAN_RX GPIO_NUM_16
+// RS485
+#define RS485_DE GPIO_NUM_21 // EN
+#define RS485_RO GPIO_NUM_18 // RX into ESP32
+#define RS485_DI GPIO_NUM_17 // TX from ESP32
+// RTC
+#define RTC_SCL GPIO_NUM_38
+#define RTC_SDA GPIO_NUM_39
+#define RTC_INT GPIO_NUM_40
+// Not assigned (external SH1.0 connector)
+#define NOT_ASSIGNED_1 GPIO_NUM_1
+#define NOT_ASSIGNED_2 GPIO_NUM_2
+// Used for USB CDC - do not use!!
+#define USB_D_P GPIO_NUM_19 // USB D+
+#define USB_D_N GPIO_NUM_20 // USB D-
+// Button
+#define KEY GPIO_NUM_0
 #endif
 
-#define DEFAULT_FPS 100
+#define DEFAULT_FPS 10
 
 // ===== DEBUG =====
 #define CAN_DEBUG 1
